11375ae33290c8badc7a413d920472ee14ea7c90
acruikshank <acruikshank@users.noreply.github.com>
2022-12-31 11:14:43 -0500

    Initial Commit

---
+++ Chromaton Watch App/ChromatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromatonApp.swift
+//  Chromaton Watch App
+//
+//  Created by Alex Cruikshank on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Chromaton_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
